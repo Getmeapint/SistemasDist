305607f786ed8173a10c0a14d66117deac0798f7
Manuel Freitas <137227903+Getmeapint@users.noreply.github.com>
2025-12-31 20:41:32 +0000

    update

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -10,8 +10,8 @@ from threading import Thread
 
 RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = int(os.environ.get("RABBITMQ_PORT", 5672))
-RABBITMQ_USER = os.environ.get("RABBITMQ_USER", "guest")
-RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "guest")
+RABBITMQ_USER = os.environ.get("RABBITMQ_USER", "grupo5")
+RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "gMEEEs4jg9")
 
 BASE_TOPIC_PREFIX = "race-"   
 
